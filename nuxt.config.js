828f6cd7f8f144c4c9d61dcc211953cadb11f460
Adam Kasper <adam@obvio.cz>
2021-10-14 17:17:51 +0200

    nuxt 3 bridge

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,6 @@
-export default {
+import { defineNuxtConfig } from '@nuxt/bridge'
+
+export default defineNuxtConfig({
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
@@ -75,4 +77,4 @@ export default {
       },
     },
   },
-}
+})
